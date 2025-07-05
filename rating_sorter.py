76a836feb49c0f1ccac50d056d8a877c00db023a
sobir0630 <mamasoliyevs538@gmail.com>
2025-07-06 08:19:28 +1030

    bajarildi tuliq

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,15 @@
 
+import csv
+
+with open('students.csv') as f:
+    reader = csv.DictReader(f)
+    students = list(reader)
+
+sorted_students = sorted(students, key=lambda x: int(x['Score']), reverse=True)
+
+with open('rating.csv', 'w', newline='') as f2:
+    fieldnames = ['Name', 'Score']
+    writer = csv.DictWriter(f2, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerows(sorted_students)
